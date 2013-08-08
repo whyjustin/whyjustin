3870ce9f5670784fe1a9ecd650492a12a3d47075
Justin Young <jyoung@sonatype.com>
2013-08-08 09:53:30 -0700

    Escape single quotes for first post

--- terminal/templatetags/template_tags.py
+++ terminal/templatetags/template_tags.py
@@ -5,15 +5,14 @@ from django.utils.text import normalize_newlines
 
 register = template.Library()
 
-def remove_newlines(text):
+def format_for_js(text):
     """
     Removes all newline characters from a block of text.
     """
     # First normalize the newlines using Django's nifty utility
     normalized_text = normalize_newlines(text)
     # Then simply remove the newlines like so.
-    return mark_safe(normalized_text.replace('\n', ' '))
-    
-remove_newlines.is_safe = True
-remove_newlines = stringfilter(remove_newlines)
-register.filter(remove_newlines)
+    return mark_safe(normalized_text.replace('\n', ' ').replace("'", "\\'"))
+format_for_js.is_safe = True
+format_for_js = stringfilter(format_for_js)
+register.filter(format_for_js)
